e363f5a4d0d2992327fd33ddaa1f48a0f4271398
christineseo <51066026+christineseo@users.noreply.github.com>
2020-03-25 18:53:18 -0400

    Add files via upload
    new versions of p_detail, javascript, and cart html

--- homework_6/myScript.js
+++ homework_6/myScript.js
@@ -23,3 +23,50 @@ function vanilla(){
 function one(x){
     document.getElementById('amount').innerHTML=x
 }
+
+//cart functionality 
+var cart = [];
+
+var Item = function(name,price,amount){
+    this.name = name
+    this.price = price
+    this.amount = amount
+}
+
+//adding to cart 
+function addItemToCart(name,price,amount){
+    for (var i in cart){
+        if (cart[i].name === name){
+            cart[i].amount += amount;
+            saveCart();
+            return;
+        }
+    }
+
+    var item = new Item(name, price, amount);
+    cart.push(item);
+    saveCart();
+}
+
+//removing from cart 
+function removeItemFromCartAll(name){
+    for (var i in cart){
+        if (cart[i].name === name){
+                cart.splice(i,1);
+            }
+            break;
+        }
+        saveCart(); 
+    }    
+
+function removeItem() {
+ var elem = document.getElementById('item');
+    elem.parentNode.removeChild(elem);
+    return false;
+    }
+
+function removeItem1() {
+ var elem = document.getElementById('item1');
+    elem.parentNode.removeChild(elem);
+    return false;
+    }
